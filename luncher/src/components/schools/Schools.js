41c97f25cc00939b3d969ed8d3ce277cae3e6fea
sjeremich23 <51142646+sjeremich23@users.noreply.github.com>
2019-11-19 09:55:24 -0700

    Added school list from API and deleted dummy data for schools

--- luncher/src/components/schools/Schools.js
+++ luncher/src/components/schools/Schools.js
@@ -1,25 +1,31 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import schoolInformation from "../../data/data";
+// import schoolInformation from "../../data/data";
 import SchoolCard from "./SchoolCard";
 
+const SchoolsStyleDiv = {
+  display: "grid",
+  gridGap: "1rem",
+  gridTemplateColumns: "repeat(auto-fill, 390px)",
+  fontSize: "15px",
+  margin: " auto",
+  justifyContent: "center",
+  width: "90%"
+};
+
 const Schools = () => {
-  const [school] = useState(schoolInformation);
+  const [school, setSchool] = useState([]);
 
   useEffect(() => {
     axios
-      // .get("https://reqres.in/api/users")
-      .get("https://lambdaluncher.herokuapp.com/api/schools")
+      .get("http://lambdaluncher.herokuapp.com/api/schools")
       .then(res => {
-        // setSchool(response.data);
+        setSchool(res.data);
         // eslint-disable-next-line no-console
-        console.log(res);
       })
       .catch(err => {
         // eslint-disable-next-line no-console
         console.log(err, err.response);
-
-        // console.log(err.response);
       });
   }, []);
 
@@ -31,16 +37,5 @@ const Schools = () => {
     </div>
   );
 };
-
-const SchoolsStyleDiv = {
-  display: 'grid',
-  
-  gridGap: '1rem',
-  gridTemplateColumns: 'repeat(auto-fill, 390px)',
-  fontSize: '15px',
-  margin: ' auto',
-  justifyContent: 'center',
-  width: '90%'
-}
 
 export default Schools;
